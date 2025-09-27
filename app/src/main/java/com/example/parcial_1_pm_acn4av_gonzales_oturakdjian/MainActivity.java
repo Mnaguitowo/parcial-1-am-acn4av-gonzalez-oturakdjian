75a3d44d81b93e780c367e7205d0b5d865d79bd9
Manguito <manu.gonzalez4738@gmail.com>
2025-09-27 19:09:01 -0300

    feat: agregar barra de nav

--- app/src/main/java/com/example/parcial_1_pm_acn4av_gonzales_oturakdjian/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_pm_acn4av_gonzales_oturakdjian/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this, "Inicio", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_offers:
+                        Toast.makeText(MainActivity.this, "Ofertas", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_cart:
+                        Toast.makeText(MainActivity.this, "Carrito", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_box:
+                        Toast.makeText(MainActivity.this, "Pedidos", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_menu:
+                        Toast.makeText(MainActivity.this, "Más", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
